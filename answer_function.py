cd765245071e171f1fbeea14c9adf17ca0e51ae5
youngG124 <youngG124@naver.com>
2022-04-15 11:36:55 +0900

    하..

---
+++ answer_function.py
@@ -0,0 +1,176 @@
+import pandas as pd
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import re
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding ='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stderr.detach(), encoding ='utf-8')
+
+train_data = pd.read_csv('rectified0412.csv')
+print(len(train_data))
+
+questions = []
+for sentence in train_data['Q']:
+    # 구두점에 대해서 띄어쓰기
+    # ex) 12시 땡! -> 12시 땡 !
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = sentence.strip()
+    questions.append(sentence)
+
+answers = []
+for sentence in train_data['A']:
+    # 구두점에 대해서 띄어쓰기
+    # ex) 12시 땡! -> 12시 땡 !
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = sentence.strip()
+    answers.append(sentence)
+
+# 서브워드텍스트인코더를 사용하여 질문과 답변을 모두 포함한 단어 집합(Vocabulary) 생성
+tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+    questions + answers, target_vocab_size=2**13)
+
+# 시작 토큰과 종료 토큰에 대한 정수 부여.
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+
+# 시작 토큰과 종료 토큰을 고려하여 단어 집합의 크기를 + 2
+VOCAB_SIZE = tokenizer.vocab_size + 2
+
+print('시작 토큰 번호 :',START_TOKEN)
+print('종료 토큰 번호 :',END_TOKEN)
+print('단어 집합의 크기 :',VOCAB_SIZE)
+
+# 서브워드텍스트인코더 토크나이저의 .encode()와 decode() 테스트해보기
+
+# 임의의 입력 문장을 sample_string에 저장
+sample_string = questions[20]
+
+# encode() : 텍스트 시퀀스 --> 정수 시퀀스
+tokenized_string = tokenizer.encode(sample_string)
+print ('정수 인코딩 후의 문장 {}'.format(tokenized_string))
+
+# decode() : 정수 시퀀스 --> 텍스트 시퀀스
+original_string = tokenizer.decode(tokenized_string)
+print ('기존 문장: {}'.format(original_string))
+
+print('hello test 1')
+
+# 최대 길이를 40으로 정의
+MAX_LENGTH = 40
+
+# 토큰화 / 정수 인코딩 / 시작 토큰과 종료 토큰 추가 / 패딩
+def tokenize_and_filter(inputs, outputs):
+  tokenized_inputs, tokenized_outputs = [], []
+  
+  for (sentence1, sentence2) in zip(inputs, outputs):
+    # encode(토큰화 + 정수 인코딩), 시작 토큰과 종료 토큰 추가
+    sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+
+    tokenized_inputs.append(sentence1)
+    tokenized_outputs.append(sentence2)
+  
+  # 패딩
+  tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_inputs, maxlen=MAX_LENGTH, padding='post')
+  tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_outputs, maxlen=MAX_LENGTH, padding='post')
+  
+  return tokenized_inputs, tokenized_outputs
+
+print('hello test 1.1')
+
+questions, answers = tokenize_and_filter(questions, answers)
+
+print('hello test 1.2')
+
+# 텐서플로우 dataset을 이용하여 셔플(shuffle)을 수행하되, 배치 크기로 데이터를 묶는다.
+# 또한 이 과정에서 교사 강요(teacher forcing)을 사용하기 위해서 디코더의 입력과 실제값 시퀀스를 구성한다.
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+print('hello test 1.4')
+
+# 디코더의 실제값 시퀀스에서는 시작 토큰을 제거해야 한다.
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': questions,
+        'dec_inputs': answers[:, :-1] # 디코더의 입력. 마지막 패딩 토큰이 제거된다.
+    },
+    {
+        'outputs': answers[:, 1:]  # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다.
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+print('hello test 1.5')
+
+tf.keras.backend.clear_session()
+
+# Hyper-parameters
+NUM_LAYERS = 2
+D_MODEL = 256
+NUM_HEADS = 8
+DFF = 512
+DROPOUT = 0.1
+
+print('hello test 2')
+
+load = tf.saved_model.load('model0413')
+
+print('hello test 3')
+
+model = load(
+    vocab_size=VOCAB_SIZE,
+    num_layers=NUM_LAYERS,
+    dff=DFF,
+    d_model=D_MODEL,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+
+print('hello test 4')
+
+def evaluate(sentence):
+    print('evaluating...' + sentence)
+    sentence = preprocess_sentence(sentence)
+
+    sentence = tf.expand_dims(
+        START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0
+    )
+
+    output = tf.expand_dims(START_TOKEN, 0)
+
+    for i in range(MAX_LENGTH):
+        predictions = model(inputs=[sentence, output], training=False)
+
+        predictions = predictions[:, -1:, :]
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        if tf.equal(predicted_id, END_TOKEN[0]):
+            break
+
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0)
+
+def predict(sentence):
+    prediction = evaluate(sentence)
+
+    print('predicting..')
+
+    predicted_sentence = tokenizer.decode(
+        [i for i in prediction if i < tokenizer.vocab_size]
+    )
+
+    print(predicted_sentence)
+
+    return predicted_sentence
+
+def preprocess_sentence(sentence):
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = sentence.strip()
+    return sentence
--- serving.py
+++ serving.py
@@ -1,9 +1,12 @@
 import numpy as np
 import tensorflow as tf
-from flask import Flask, request
+import os
+
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding ='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stderr.detach(), encoding ='utf-8')
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 load = tf.saved_model.load('model0413')
-
-print(load.summary())
-
-# 
